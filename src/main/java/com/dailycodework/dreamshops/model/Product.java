6a7422e885f88ec18dc76c8bf862df517c7f0b5c
Anny Dang <arianne.dangvankhanh@gmail.com>
2025-04-22 14:08:58 -0400

    first day

---
+++ src/main/java/com/dailycodework/dreamshops/model/Product.java
@@ -0,0 +1,21 @@
+package com.dailycodework.dreamshops.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToMany;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    private Category category;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+}
